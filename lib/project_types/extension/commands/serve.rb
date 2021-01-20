ebd29b1b553c431aaf7a133904778b385f4415d0
Phiroze Noble <phiroze.dennis@shopify.com>
2021-01-20 14:14:52 -0500

    Output development store url on extensions serve

--- lib/project_types/extension/commands/serve.rb
+++ lib/project_types/extension/commands/serve.rb
@@ -7,7 +7,15 @@ module Extension
       NPM_SERVE_COMMAND = %w(run-script server)
 
       def call(_args, _command_name)
+        if ShopifyCli::Shopifolk.check
+          ShopifyCli::Tasks::EnsureEnv.call(@ctx, required: [:api_key, :secret, :shop])
+          ShopifyCli::Tasks::EnsureDevStore.call(@ctx)
+        end
+
         CLI::UI::Frame.open(@ctx.message('serve.frame_title')) do
+          if(ShopifyCli::Shopifolk.check) 
+            @ctx.puts("#{construct_url}")
+          end
           success = ShopifyCli::JsSystem.call(@ctx, yarn: YARN_SERVE_COMMAND, npm: NPM_SERVE_COMMAND)
           @ctx.abort(@ctx.message('serve.serve_failure_message')) unless success
         end
@@ -18,6 +26,12 @@ module Extension
           Serve your extension in a local simulator for development.
             Usage: {{command:#{ShopifyCli::TOOL_NAME} serve}}
         HELP
+      end
+
+      private 
+
+      def construct_url 
+        "https://#{project.env.to_h['SHOP'].to_s}/admin/extension-dev"
       end
     end
   end
